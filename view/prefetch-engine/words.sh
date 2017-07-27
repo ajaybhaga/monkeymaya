b41506d186fad016434cffebb6d422551f172fc1
Ajay Bhaga <ajay.bhaga@gmail.com>
2017-07-26 23:19:06 -0400

    Rendered images.

--- view/prefetch-engine/words.sh
+++ view/prefetch-engine/words.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-for word in blissful dreamy defeated furious psychedelic sexy seductive euphoric raging vibrant
-do
-echo $word
-./genWord.sh $word
+
+for extraword in red blue purple black electric fuzzy shiny metal organic plastic earth planet universe
+do  
+  for word in blissful dreamy defeated furious psychedelic sexy seductive euphoric raging vibrant
+  do
+    echo word=${word}, extraword=${extraword}
+    ./genWord.sh ${word} ${extraword}
+  done
 done
 exit 0
 
